296cb7aee4fb50d5b0ff4f7f7d856258da8b8afd
Quirinr <quirin.renggli@outlook.com>
2025-04-01 11:57:33 +0200

    added Lock-in calculations, but problem
    R and Theta not const eventhough shorted output-input: => the 2 sines do not align superwell, plot them and see what you can do

--- Lock_in.m
+++ Lock_in.m
@@ -10,7 +10,7 @@ Settings.get_sample_T = ''; % {'', 'Lakeshore336', 'Lakeshore325', 'Oxford_ITC'}
 Settings.type = 'Timetrace'; % (.type influences only plot labels)
 Settings.ADwin = 'ProII'; % GoldII or ProII
 
-Timetrace.runtime = 50;      % s
+Timetrace.runtime = 10;      % s
 Timetrace.scanrate = 500000;       % Hz
 Timetrace.points_av = 100;        % points
 Timetrace.process_number = 2;
@@ -57,15 +57,15 @@ Set_Processdelay(2, Timetrace.process_delay);
 %% set up sinewave
 
 Processdelay = Get_Processdelay(2);
-f_wanted = 200;
-phi_shift = 0; %phase shift in radian change to degrees
+f_wanted = 7;
+phi_shift = 0; %phase shift in degrees
 Amplitude = 1; %Amplitude of sinewave
 
 f_process = Settings.clockfrequency/(Processdelay * 10); % *10 because in ADBASIC only every 10th processcycle the voltage gets changed
 wave_vec_length = f_process/f_wanted;
 wave_vec_length = round(wave_vec_length)
 q = 1:wave_vec_length;
-wave = Amplitude * sin(q*2*pi/wave_vec_length + phi_shift); %NOTE: Rounding Error, TODO: calculate it
+wave = Amplitude * sin(q*2*pi/wave_vec_length + phi_shift*2*pi/360); %NOTE: Rounding Error, TODO: calculate it
 wave_bin = convert_V_to_bin(wave, Settings.output_min, Settings.output_max, Settings.output_resolution);
 
 SetData_Double(1, wave_bin, 0);
@@ -83,24 +83,34 @@ Settings.N_ADC = 1;
 Settings.ADC_idx = 1;
 Timetrace = Realtime_timetrace(Settings, Timetrace, Settings.type);
 
-%% Processed Data
+%% Processing Data
+measured_signal = GetData_Double(2, 0, Timetrace.sampling_rate * Timetrace.runtime); %extracts input signal from Adwin
 
-%Idea: since sine is set at 50kHz but measured signal at 5kHz we can only
-%multiply them after downsampling our sine:
+%Idea: since sine is set at 50kHz but measured signal at 5kHz -> sine must be changed accordingly
+%calculates how many datapoints in measured_signal represent 1 period
+period_length = wave_vec_length * Timetrace.sampling_rate / 50000; % the 50000 stands for the rate at which the individual wave entries are applied (voltage steps)
+q = 1:length(measured_signal);%helper for sine multiplication
 
-%wave = wave(1:10:end);
+%mixes measured_signal with a sine of same freq as our initial wave
+mixed_signal_inphase = measured_signal * Amplitude .* sin(q*2*pi/period_length + phi_shift*2*pi/360);
+mixed_signal_quadrature = measured_signal * Amplitude .* sin(q*2*pi/period_length + pi/2 + phi_shift*2*pi/360);
+
+%% Lock-in calculations
+
+filtered_signal_inphase = lowpass(mixed_signal_inphase, 0.001, Timetrace.sampling_rate);
+filtered_signal_quadrature = lowpass(mixed_signal_quadrature, 0.001, Timetrace.sampling_rate);
+
+R = sqrt(filtered_signal_inphase.^2 + filtered_signal_quadrature.^2);
+Theta = atan(filtered_signal_quadrature ./filtered_signal_inphase);
 
 
+%hold on
+%plot(abs(fft(measured_signal)), LineStyle="-", Color= 'w')
+%plot(abs(fft(mixed_signal)), LineStyle='-', Color='b')
+%plot(abs(fft(filtered_signal_inphase)), LineStyle='-', Color='r')
+%hold off
 
-%measured_signal = GetData_Double(2, 0, Timetrace.sampling_rate * Timetrace.runtime); %extracts input signal from Adwin
-
-
-
-%lowpass(input_signal, 150 ,Timetrace.sampling_rate)
-
-%mixed_signal = input_signal *
-
-
-
+%plot(R(1:2000))
+%plot(Theta(1:2000))
 
 
