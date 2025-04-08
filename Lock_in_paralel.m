957dcd29b33d8489fda491781b6e76bed2fc03cd
Quirinr <quirin.renggli@outlook.com>
2025-04-08 11:35:11 +0200

    added offline filtering

--- Lock_in_paralel.m
+++ Lock_in_paralel.m
@@ -30,39 +30,25 @@ Settings = Init(Settings);
 Settings = Init_ADwin(Settings, Waveform, Timetrace);
 
 %% set up sinewave
-Set_Processdelay(6, 2000); %500kHz on ProII, standard for timetrace
-Processdelay = Get_Processdelay(6);
 
-f_wanted = 125;      
+f_wanted = 200;      
 phi_shift = 0; %phase shift in degrees
 Amplitude = 1;
+wave_vec_length = 1000;
 
-f_process = Settings.clockfrequency/(Processdelay);
-wave_vec_length = f_process/f_wanted;
-wave_vec_length = round(wave_vec_length)
 q = 1:wave_vec_length;
-wave = Amplitude * sin(q*2*pi/wave_vec_length + phi_shift*2*pi/360); %NOTE: Rounding Error, TODO: calculate it
+wave = Amplitude * sin(q*2*pi/wave_vec_length + phi_shift*2*pi/360);
 wave_bin = convert_V_to_bin(wave, Settings.output_min, Settings.output_max, Settings.output_resolution);
 
-
-%compressing Wave such that only unique bins get loaded up and introducing a compression vector which contains the amount of discarded values each
-shifted_wave_bin = [0, wave_bin(1:end-1)];
-residuals = wave_bin - shifted_wave_bin;%by subtracting a shifted version, all elements that are the same as its previous one get set to 0
-
-
-indexes = find(residuals ~= 0); %finds the indexes of all non 0 elements
-indexes = [indexes, length(residuals)+1]; %pads it
-compression_vector = -1* (indexes(1:end-1) - indexes(2:end)); %contains the number of times the voltage at this point should be repeated
-
-residuals(residuals ~= 0) = 1; %sets all non 0 elements to 1: prepares for multiplication
-wave_bin_compressed = wave_bin .* residuals;%sets all repeating wave_bins to 0
-wave_bin_compressed(wave_bin_compressed == 0) = []; %the compressed voltage wave
+Processdelay6 = round(Settings.clockfrequency/(wave_vec_length * f_wanted));
+actual_f_wanted = Settings.clockfrequency/(wave_vec_length * Processdelay6);
+fprintf("actual frequency = %f \n", actual_f_wanted)
+Set_Processdelay(6, Processdelay6); %500kHz on ProII, standard for timetrace
 
 
-SetData_Double(1, wave_bin_compressed, 0);
-SetData_Double(3, compression_vector, 0);
+SetData_Double(1, wave_bin, 0);
 Set_Par(8, Waveform.output);
-Set_Par(23,numel(wave_bin));
+Set_Par(23, numel(wave_bin));
 Set_Par(6, Settings.AO_address);
 
 %% set up timetrace
@@ -104,6 +90,60 @@ Settings.N_ADC = 1;
 Settings.ADC_idx = 1; %changed it to 2 since 1 was really weird
 Timetrace = Realtime_timetrace(Settings, Timetrace, Settings.type);
 
-%% Processing Data
 
+%% Mixing
+measured_signal = GetData_Double(2, 0, Timetrace.sampling_rate * Timetrace.runtime); %extracts input signal from Adwin
+
+%Idea: since sine is set at 50kHz but measured signal set at different frequency -> sine must be changed accordingly
+%calculates how many datapoints in measured_signal represent 1 period
+period_length = wave_vec_length * Timetrace.sampling_rate * Processdelay6 /Settings.clockfrequency% Lvoltage * samplingrate/Voltagesettingfreq
+q = 1:length(measured_signal);%helper for sine multiplication
+
+timeshift = Get_Par(27); %shift of reference signal to measuring begin
+%initial_phase_shift = timeshift/wave_vec_length * 360;
+q = q + timeshift;
+
+%mixes measured_signal with a sine of same freq as our initial wave + the initial shift due to parallel implementation
+mixed_signal_inphase = measured_signal * Amplitude .* sin(q*2*pi/period_length + phi_shift*2*pi/360);
+mixed_signal_quadrature = measured_signal * Amplitude .* sin(q*2*pi/period_length + pi/2 + phi_shift*2*pi/360);
+
+%% Lock-in calculations
+
+%filtered_signal_inphase = lowpass(mixed_signal_inphase,   1, Timetrace.sampling_rate); %non optimal, design filter yourself
+%filtered_signal_quadrature = lowpass(mixed_signal_quadrature, 1, Timetrace.sampling_rate); %non optimal, design filter yourself
+
+cutoff = 1;
+
+% Design a 4th-order Butterworth lowpass filter
+[b, a] = butter(4, cutoff / (Timetrace.sampling_rate / 2), 'low');
+
+% Apply the filter to remove high frequencies
+filtered_signal_inphase = filtfilt(b, a, mixed_signal_inphase);  % Zero-phase filtering
+filtered_signal_quadrature = filtfilt(b, a, mixed_signal_quadrature);
+
+R = sqrt(filtered_signal_inphase.^2 + filtered_signal_quadrature.^2);
+Theta = atan(filtered_signal_quadrature ./filtered_signal_inphase);
+
+%% plotting
+
+filtered_signal_inphase_RMS = filtered_signal_inphase/sqrt(2);
+filtered_signal_quadrature_RMS = filtered_signal_quadrature/sqrt(2);
+
+hold on
+%plot(abs(fft(measured_signal)), LineStyle="-", Color= 'w')
+%plot(abs(fft(mixed_signal_inphase)), LineStyle='-', Color='b')
+%plot(abs(fft(filtered_signal_inphase)), LineStyle='-', Color='r')
+hold off
+
+
+hold on
+%plot(filtered_signal_inphase(1:2*round(period_length)))
+%plot(filtered_signal_quadrature(1:2*round(period_length)))
+%plot(Theta(1:2*round(period_length)))
+%plot(R(1:2*round(period_length)))
+%plot(R)
+%plot(Theta)
+%plot(filtered_signal_inphase(1:15*period_length))
+%plot(mixed_signal_inphase(1:500))
+hold off
 
